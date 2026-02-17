6ca80624ed5fa8ff24683926aa759734d408b01a
AmauryRomero1285 <230190@utxicotepec.edu.mx>
2026-02-17 17:25:19 -0600

    New updates
    Migration to react

---
+++ DevCorp-React/src/js/tailwindConfig.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+      darkMode: "class",
+      theme: {
+        extend: {
+          colors: {
+            primary: "#1b657e",
+            "background-light": "#f9fafb",
+            "background-dark": "#21262c",
+            "accent-orange": "#f97316",
+          },
+          fontFamily: {
+            display: ["Manrope", "sans-serif"],
+          },
+          borderRadius: {
+            DEFAULT: "0.25rem",
+            lg: "0.5rem",
+            xl: "0.75rem",
+            full: "9999px",
+          },
+        },
+      },
+    };
